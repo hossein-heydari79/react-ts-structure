e3379130131c4e6f413b3909a2508c8367a25448
hossein-heydari <hosseinheydari790228@gmail.com>
2022-11-14 23:26:19 +0330

    'test'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './App.module.scss'
 
 function App() {
-  return <div className={styles.app}>hossein</div>
+return <div className={styles.app}>hossein</div>
 }
 
 export default App
