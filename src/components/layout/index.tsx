ec5a3ac889ee152eb19e936400c65a7a56bf61a4
hossein-heydari <hosseinheydari790228@gmail.com>
2022-11-18 14:31:03 +0330

    add display utilities

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,16 +3,15 @@ import styles from './layout.module.scss'
 function Layout() {
   return (
     <div
+      className='d-flex'
       style={{
         width: '300px',
         height: '300px',
-        border: '1px solid black',
-        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <p className='ml-auto' style={{ width: 'fit-content', height: '50px' }}>
-        hossein
-      </p>
+      <p>hossein</p>
     </div>
   )
 }
