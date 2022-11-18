e76cede824a425dd41416b620a6338bb56e3c348
hossein-heydari <hosseinheydari790228@gmail.com>
2022-11-18 23:09:37 +0330

    add width and height utilities

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,13 +2,7 @@ import styles from './layout.module.scss'
 
 function Layout() {
   return (
-    <div
-      className='d-flex'
-      style={{
-        width: '300px',
-        height: '300px',
-      }}
-    >
+    <div className='d-flex w-50'>
       <p className={styles['name']}>hossein</p>
     </div>
   )
