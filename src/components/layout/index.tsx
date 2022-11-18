b1602eb24f67a27c7068cea061b98bdb4c95fcb1
hossein-heydari <hosseinheydari790228@gmail.com>
2022-11-18 23:59:50 +0330

    add alignments utilities

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,14 @@
 // import styles from './layout.module.scss'
 
 function Layout() {
-  return <div className='w-50 text-center'>hossein</div>
+  return (
+    <div
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: '300px', width: '300px' }}
+    >
+      <p>hossein</p>
+    </div>
+  )
 }
 
 export default Layout
