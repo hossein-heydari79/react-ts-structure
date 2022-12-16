2df93cd90ec0107544d8ae14b8749c10d6a2a91b
hossein-heydari <hosseinheydari790228@gmail.com>
2022-12-16 21:21:11 +0330

    change layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,11 +6,13 @@ import styles from './layout.module.scss'
 function Layout() {
   return (
     <div className={styles['layout']}>
+      <h1>header</h1>
       <Routes>
         {webRoutes.map(({ element, path }) => (
           <Route path={path} element={element} />
         ))}
       </Routes>
+      <h1>footer</h1>
     </div>
   )
 }
